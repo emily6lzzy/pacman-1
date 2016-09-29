a549bb16ad07b808d33d992b5f19023bf93b6045
João Eduardo Montandon <edu.montandon@gmail.com>
2016-09-29 14:01:44 -0300

    Implemented Heuristic based on manhattan heuristic for corner search problem.

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -363,8 +363,14 @@ def cornersHeuristic(state, problem):
     corners = problem.corners # These are the corner coordinates
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
 
-    "*** YOUR CODE HERE ***"
-    return 0 # Default to trivial solution
+    distance = 0
+
+    for missingCoordinate in set(corners) - state['corners']:
+        distance += abs(state['position'][0] - missingCoordinate[0]) + \
+                    abs(state['position'][1] - missingCoordinate[1])
+
+    return distance
+    
 
 class AStarCornersAgent(SearchAgent):
     "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
